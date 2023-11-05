e00d5f43619155d542a1e41eb963411124bf72be
jinhaidi <jinhaidi@laiye.com>
2023-11-05 11:42:08 +0800

    feat: add prompts

--- bin/cli.ts
+++ bin/cli.ts
@@ -1,4 +1,4 @@
-import { getImageBySentence } from "../src/get-up";
+import { getImageBySentence, getSentence } from "../src/get-up";
 import type { Response } from "../src/types";
 import path from "path";
 import fs from "fs";
@@ -7,69 +7,78 @@ import { promisify } from "util";
 
 const pipeline = promisify(stream.pipeline);
 
+const prompts = [
+  // Oil painting style:
+  'Inspired by the Chinese ancient poem "$title$", create an oil painting-style artwork that reflects the poetic charm and characters.',
+  // Realistic style:
+  'Using the Chinese ancient poem "$title$" as the source of inspiration, draw a realistic-style picture that highlights the scene details and atmosphere.',
+  // Cyberpunk style:
+  'Combine the Chinese ancient poem "$title$" with cyberpunk elements to design a unique image blending tradition and sci-fi.',
+];
 async function init() {
   const cwd = process.cwd();
 
   const argv = require("minimist")(process.argv.slice(2));
+  const cookie = argv.cookie;
+  const sentence = await getSentence();
 
-  if (argv.cookie) {
-    try {
-      const res: Response = await getImageBySentence(argv.cookie);
-      console.log("Create Successful: ", res);
+  console.log("getSentence Result: ", sentence);
+  const createImagePromises = prompts.map(async (prompt) => {
+    sentence.content = prompt.replace("$title$", sentence.content);
+    const res: Response = await getImageBySentence(cookie, sentence);
+    console.log("Create Successful: ", res);
 
-      const outputPath = path.join(cwd, "public");
+    const outputPath = path.join(cwd, "public");
 
-      const imagesPath = path.join(outputPath, "images");
-      if (!fs.existsSync(imagesPath)) {
-        fs.mkdirSync(imagesPath);
-      }
+    const imagesPath = path.join(outputPath, "images");
+    if (!fs.existsSync(imagesPath)) {
+      fs.mkdirSync(imagesPath);
+    }
 
-      // 在 images 目录下，创建一个以时间戳命名的文件夹，将图片放入其中
-      const imagesFolderName = Date.now().toString();
-      const imagesFolderPath = path.join(imagesPath, imagesFolderName);
-      if (!fs.existsSync(imagesFolderPath)) {
-        fs.mkdirSync(imagesFolderPath);
-      }
+    const imagesFolderName = Date.now().toString();
+    const imagesFolderPath = path.join(imagesPath, imagesFolderName);
+    if (!fs.existsSync(imagesFolderPath)) {
+      fs.mkdirSync(imagesFolderPath);
+    }
+    await writeContent(res, imagesFolderName);
+    console.log("writeContent success: ", imagesFolderName);
 
-      // 将图片放入 images 目录下的文件夹中
-      res.images.forEach((image, index) => {
-        // images 中是网络url，请求图片，将图片保存到 images 目录下的文件夹中
-        const imageFileName = `${index}.jpg`;
-        const imageFilePath = path.join(imagesFolderPath, imageFileName);
+    const downImagePromises = res.images.map((image, index) => {
+      const imageFileName = `${index}.jpg`;
+      const imageFilePath = path.join(imagesFolderPath, imageFileName);
 
-        // 下载图片
-        fetch(image).then((res) => {
-          if (!res.ok) throw new Error(`unexpected response ${res.statusText}`);
-          // @ts-ignore
-          pipeline(res.body, fs.createWriteStream(imageFilePath)).catch((e) => {
-            console.error("Something went wrong while saving the image", e);
-          });
+      return fetch(image).then((res) => {
+        if (!res.ok) throw new Error(`unexpected response ${res.statusText}`);
+        // @ts-ignore
+        pipeline(res.body, fs.createWriteStream(imageFilePath)).catch((e) => {
+          console.error("Something went wrong while saving the image", e);
         });
       });
-      const options = { timeZone: "Asia/Shanghai", hour12: false };
-      const outputData = {
-        ...res,
-        date: new Date().toLocaleString("zh-CN", options),
-        localImagesPath: imagesFolderName,
-      };
-
-      const contentPath = path.join(cwd, "public/content");
-
-      const contentFile = path.join(contentPath, `${imagesFolderName}.json`);
-
-      fs.writeFileSync(contentFile, JSON.stringify(outputData));
-
-      setTimeout(() => {
-        // 为了让图片下载完毕，再退出进程
-        process.exit(0);
-      }, 5000);
-    } catch (e) {
-      throw e;
-    }
-  } else {
-    throw new Error("Please provide a cookie using the --cookie argument");
-  }
+    });
+    return Promise.all(downImagePromises);
+  });
+  await Promise.all(createImagePromises);
+  console.log("createImages success");
+  process.exit(0);
 }
+
+function writeContent(res, imagesFolderName) {
+  const cwd = process.cwd();
+  const contentPath = path.join(cwd, "public/content");
+  return new Promise((resolve) => {
+    const options = { timeZone: "Asia/Shanghai", hour12: false };
+    const outputData = {
+      ...res,
+      date: new Date().toLocaleString("zh-CN", options),
+      localImagesPath: imagesFolderName,
+    };
+    const contentFile = path.join(contentPath, `${imagesFolderName}.json`);
+    console.log({ contentFile, contentPath });
+    fs.writeFileSync(contentFile, JSON.stringify(outputData));
+    resolve(true);
+  });
+}
+// writeContent({ a: "s" }, "aaaa");
 
 init().catch((e) => {
   console.error(e);
--- src/get-up.ts
+++ src/get-up.ts
@@ -9,7 +9,7 @@ import type { SentenceResponse, Response } from "./types";
  * @returns SentenceResponse
  * @throws {Error} The error
  **/
-async function getSentence(): Promise<SentenceResponse> {
+export async function getSentence(): Promise<SentenceResponse> {
   try {
     const res = await fetch(SENTENCE_API);
     const data: SentenceResponse = await res.json();
@@ -19,27 +19,22 @@ async function getSentence(): Promise<SentenceResponse> {
   }
 }
 
-async function getImageBySentence(cookie: string): Promise<Response> {
+async function getImageBySentence(
+  cookie: string,
+  sentence: SentenceResponse
+): Promise<Response> {
   const bingImageCreator = new BingImageCreator({
     cookie: cookie,
   });
 
-  const res = await getSentence();
-  console.log("getSentence Result: ", res);
-
-  const prompt = `${res.content}, textless`;
-  try {
-    const images = await bingImageCreator.createImage(prompt);
-    return {
-      images,
-      content: res.content,
-      origin: res.origin,
-      author: res.author,
-      category: res.category,
-    };
-  } catch (error) {
-    throw new Error("Bing Image create failed: ", error);
-  }
+  const images = await bingImageCreator.createImage(sentence.content);
+  return {
+    images,
+    content: sentence.content,
+    origin: sentence.origin,
+    author: sentence.author,
+    category: sentence.category,
+  };
 }
 
 export { getImageBySentence };
